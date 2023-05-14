7310d05c7cea9d324f94f114ffcd8cc37bf66330
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-05-14 11:12:13 -0700

    update project from template

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Haiku Checker',
       template: './src/index.html',
       inject: 'body'
     })
